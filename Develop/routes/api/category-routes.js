44c48ceacb6cd5af98782ee1347a265188ccb8f5
Adam Olson <adam.lee.olson@outlook.com>
2023-09-04 10:13:38 -0500

    working with routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,10 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try{
+    const categories = await Category.findAll()
+    res.json(categories)
+  } catch(err){
+    res.status(500).json(err)
+  }
 });
+
+// find all categories
+  // be sure to include its associated Products
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
